99d87058d6c28adcafb9d08037643a970a7c8cb7
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2019-12-22 20:24:31 +0000

    fixed new drive path duplication error

--- db-function.js
+++ db-function.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const mysql = require('mysql2/promise')
 const config = require('./db-config')
-//const connection = mysql.createConnection(config.mysql)
-const connection = mysql.createConnection(config.mysqlPi)
-//const path = '/Users/leoclarke/Documents/GitHub/Final-Year-Project/api/drive/media/'
-const path = '/media/pi/'
+const connection = mysql.createConnection(config.mysql)
+//const connection = mysql.createConnection(config.mysqlPi)
+const path = '/Users/leoclarke/Documents/GitHub/Final-Year-Project/api/drive/media/'
+//const path = '/media/pi/'
 function getMediaVolumes(){
         
         try {
@@ -17,14 +17,16 @@ function getMediaVolumes(){
 }
 
 async function refreshList(list){
-    let con = await connection
-    let [dbList] = await con.query("SELECT * FROM unaddedDrive WHERE unaddedDrive_added = ? ",[false])
     for(let i = 0; i < list.length; i++){
-        //if(list[i] == dbList[i])
-        let drivePath = path + list[i]
-        let con = await connection;
-        await con.query("INSERT INTO unaddedDrive(unaddedDrive_name, unaddedDrive_path, unaddedDrive_added) VALUES (?,?,?)", [list[i], drivePath, false]);
-
+        let con = await connection
+        let [dbList] = await con.query("SELECT unaddedDrive_name FROM unaddedDrive WHERE unaddedDrive_name = ? AND unaddedDrive_added = ? ",[list[i].toString(),false])
+        if (!dbList.length) {
+            let drivePath = path + list[i]
+            let con = await connection;
+            await con.query("INSERT INTO unaddedDrive(unaddedDrive_name, unaddedDrive_path, unaddedDrive_added) VALUES (?,?,?)", [list[i], drivePath, false]);
+        }else{
+           console.log("already added") 
+        }
     }
 }
 
@@ -32,7 +34,6 @@ module.exports.getUnaddedDriveList = async () => {
     getMediaVolumes()
     let con = await connection
     let [list] = await con.query("SELECT * FROM unaddedDrive WHERE unaddedDrive_added = ? ",[false])
-    console.log(list)
     return JSON.stringify(list)
 };
 
