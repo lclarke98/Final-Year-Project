f55de09a66cafb53de399099da681a1d49eafd98
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2019-12-22 18:13:55 +0000

    Update db.sql

--- db.sql
+++ db.sql
@@ -17,14 +17,14 @@ CREATE TABLE if not exists nas.addedDrive(
   addedDrive_name VARCHAR(64) NOT NULL,
   addedDrive_path VARCHAR(75) NOT NULL,
   addedDrive_raid BOOLEAN NOT NULL
-);,
+);
 
 CREATE TABLE if not exists nas.unaddedDrive(
   unaddedDrive_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   unaddedDrive_name VARCHAR(64) NOT NULL,
-  unaddedDrive_path VARCHAR(75) NOT NULL,
+  unaddedDrive_path VARCHAR(200) NOT NULL,
   unaddedDrive_added BOOLEAN NOT NULL
-);,
+);
 
 use nas
 
