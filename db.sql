06511edf5c214a2e1e7c38eb4cbf67f432041813
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-01-21 15:44:21 +0000

    added user id

--- db.sql
+++ db.sql
@@ -1,8 +1,10 @@
 CREATE DATABASE if not exists nas;
 
 CREATE TABLE if not exists nas.user(
-  user_name VARCHAR(64) NOT NULL PRIMARY KEY,
-  user_password VARCHAR(64) NOT NULL
+  user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  user_name VARCHAR(64) NOT NULL,
+  user_password VARCHAR(64) NOT NULL,
+  CONSTRAINT user_unique UNIQUE (user_name)
 );
 
 CREATE TABLE if not exists nas.unaddedDrive(
@@ -24,10 +26,10 @@ CREATE TABLE if not exists nas.addedDrive(
 CREATE TABLE if not exists nas.permissions(
   permission_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   addedDrive_name VARCHAR(64) NOT NULL,
-  user_name VARCHAR(64) NOT NULL,
+  user_id INT NOT NULL,
   permission_read BOOLEAN NOT NULL,
   permission_write BOOLEAN NOT NULL,
-  FOREIGN KEY (user_name) REFERENCES user(user_name) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE,
   FOREIGN KEY (addedDrive_name) REFERENCES addedDrive(addedDrive_name) ON DELETE CASCADE
 );
 
@@ -44,3 +46,4 @@ use nas
 insert into user (user_name, user_password) values ('admin', 'admin');
 insert into user (user_name, user_password) values ('root', 'root');
 insert into user (user_name, user_password) values ('lclarke', 'lclarke');
+
