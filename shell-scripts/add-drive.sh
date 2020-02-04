b3d932916d4f6ef5866270889af38817a1f1fc3a
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-02-04 22:58:41 +0000

    Update add-drive.sh

--- shell-scripts/add-drive.sh
+++ shell-scripts/add-drive.sh
@@ -3,46 +3,35 @@
 #KEY
 # $1 drive path
 # $2 drive name
+# $3 permissions
 
 newPath="/media/pi/"+$2
 
 # Step 0 create mount point
-
 sudo mkdir /home/pi/nas-mount/$2
 
-# Step get sda and add mount point to fstab
-
-getPath="$(ls -l /dev/disk/by-label/MJRO1815 | tail -c 6)"
-
-sd="${getPath}" 
-
-sudo echo "/dev$sd /home/pi/nas-mount$2 ext4 defaults 0 0" >> "/ect/fstab"
-
-echo $sd
-
-# Step one umount the drive
-
-sudo umount -a $1
-
-# Step two format drive with new name
-
+# Format drive and rename
 sudo mkfs.ext4 /dev/sda1 -L $2
 
-# Step three mount the drive
+# Step get sda and add mount point to fstab
+getPath="$(ls -l /dev/disk/by-label/MJRO1815 | tail -c 6)"
+sd="${getPath}" 
+sudo echo "/dev$sd /home/pi/nas-mount$2 ext4 defaults 0 0" >> "/ect/fstab"
 
-sudo mount $newPath
+# mount the drive
+sudo mount /home/pi/nas-mount/$2
+
 
 # Step four change the r+w permissions
 
-sudo chmod a+rwx $newPath
+sudo chmod a+rwx /home/pi/nas-mount/$2
 
-# Now add to SAMBA ????????????????????????????
+# add drive to smb config
 
 smbConf="/etc/samba/smb.conf"
-name="nas1"
-newPath="/media/pi/"$name
+newPath="/home/pi/nas-mount/$2"
 
-echo "[$name]" >> $smbConf
+echo "[$2]" >> $smbConf
 echo "path = "$newPath >> $smbConf
 echo "writeable=yes" >> $smbConf
 echo "create mask=0777" >> $smbConf
