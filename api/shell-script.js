e50047ff9ab83f9d61d84d687c2d9cdc97f8f162
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-02-08 00:06:15 +0000

    added sorting functions

--- api/shell-script.js
+++ api/shell-script.js
@@ -1,7 +1,26 @@
 const shell = require('shelljs');
+const db = require('../db-function')
+
+function sortReadList(permissions){
+    let readList
+    for(let i = 0; i > permissions.length; i++){
+        readlist + " " + permissions[i].read
+    }
+    return readList
+}
+
+function sortWriteList(permissions){
+    let writeList
+    for(let i = 0; i > permissions.length; i++){
+        writelist + " " + permissions[i].write
+    }
+    return writeList
+}
 
 module.exports.addDrive = async (driveName, drivePath, permissions) => {
-    shell.exec('sh ../shell-scripts/add-drive.sh' + drivePath, driveName, permissions)
+    const readList = sortReadList(permissions)
+    const writeList = sortWriteList(permissions)
+    shell.exec('sh ../shell-scripts/add-drive.sh' + drivePath, driveName, readList, writeList)
 };
 
 module.exports.addUser = async (userName, password, permissions) => {
