b54122c9dc039f8b9238e9d221fc5ed362ffd65e
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-02-09 00:06:07 +0000

    Create update-permissions.sh

---
+++ shell-scripts/update-permissions.sh
@@ -0,0 +1,6 @@
+# by drive 
+    # search drive replace read and write lines
+
+
+# by user
+    # recursively replace all read and write lines - maybe for loop in js calling each time with next drive and replace ^^ first comment function 
