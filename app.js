62b3487e8ecc8b0f67f39a5a02a462c98a16c8b3
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-02-11 00:06:51 +0000

    added session

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const path = require('path');
 const session = require('express-session');
 const app = express();
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
+
 app.use('/api', require('./api'));
 app.use('/user', require('./user'));
 
@@ -13,12 +20,17 @@ app.use(express.static(path.join(__dirname, 'static')));
 const port = process.env.PORT || 80;
 
 
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 60000 }
-}))
+app.get('/dashboard', async (req, res) => {
+  try {
+      const result = "login"
+      console.log(result)
+      res.redirect("/main-dashboard.html")
+  }catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
+});
+
 
 app.listen(port, (err) => {
   if (err) console.log('error', err);
