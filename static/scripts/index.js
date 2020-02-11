4bb711e4d36a4e7ca9b348c4def2fceb54c1b2b4
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-02-11 21:16:47 +0000

    Update index.js

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -6,9 +6,22 @@ function pageLoad(){
 
 
 async function login(){
-    const url = `/user/login`
+    const username = document.getElementById("username")
+    const password = document.getElementById("password")
+    const data = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          info: {
+            username: username,
+            password: password,
+          }
+        })
+      }
+    const response = await fetch('/api/newDrive', data)
     const response = await fetch(url)
     const result = await response
-    console.log(result.url)
     location.replace(result.url);
 }
