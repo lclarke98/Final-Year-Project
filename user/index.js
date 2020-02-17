5920d17ca1c199f5adcb3d8abc4343d9fc23460a
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-02-17 17:54:18 +0000

    added logout

--- user/index.js
+++ user/index.js
@@ -9,19 +9,33 @@ user.use(bodyParser.urlencoded({ extended: true }))
 
 user.get('/login', async (req, res) => {
     try {
-        const username = "admin"
-        const password = "admin"
+        const username = req.query.username
+        const password = req.query.password
+        console.log(username)
         const result = await db.loginRequest(username,password)
-        console.log(result[0].user_name)
-        //req.session = {}
-        req.session.user_id = result[0].user_id
-        req.session.user_name = result[0].user_name
-        res.redirect("/dashboard")
+        if(result.length == 0){
+            res.sendStatus(401)
+        }else{
+            req.session.user_id = result[0].user_id
+            req.session.user_name = result[0].user_name
+            res.redirect("/dashboard")
+        }
     }catch (e) {
       console.error(e);
       res.sendStatus(500);
     }
 });
+
+user.get('/logout', async (req, res) => {
+    try {
+        req.session.destroy();
+        res.redirect("/login")
+    }catch (e) {
+      console.error(e);
+      res.sendStatus(500);
+    }
+});
+
 
 user.get('/session', async (req, res) => {
     console.log(req.session)
