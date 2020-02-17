e9617ea3b23ff8335514312a22ac84d9717695bf
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-02-17 18:31:42 +0000

    Create setup.sh

---
+++ shell-scripts/setup.sh
@@ -0,0 +1,20 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+sudo apt install nodejs -y
+
+
+sudo apt-get install mysql-server -y
+sudo apt-get install mariadb-server -y
+sudo mysql_secure_installation -y
+
+sudo chmod a+rwx "/etc/fstab"
+sudo chmod a+rwx "/etc/samba/smb.conf"
+
+mkdir nas-mounts
+
+git clone "https://github.com/lclarke98/Final-Year-Project"
+cd "Final-Year-Project"
+sudo npm install
+sudo npm start
