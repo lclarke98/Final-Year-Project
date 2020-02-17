e208a1bd9278f9987a73eb6d9c606a6622090d9a
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-02-17 17:53:00 +0000

    fixed login function

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -4,24 +4,11 @@ function pageLoad(){
     document.getElementById("login-button").addEventListener('click', login)
 }
 
-
 async function login(){
-    const username = document.getElementById("username")
-    const password = document.getElementById("password")
-    const data = {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          info: {
-            username: username,
-            password: password,
-          }
-        })
-      }
-    const response = await fetch('/api/newDrive', data)
-    const response = await fetch(url)
-    const result = await response
-    location.replace(result.url);
+  const username = document.getElementById("username-input").value
+  const password = document.getElementById("password-input").value
+  const url = `/user/login?username=${username}&password=${password}`;
+  const response = await fetch(url);
+  const result = await response
+  location.replace(result.url)
 }
