fdbad1d0998667d88d47dedd9dbf6454e71b0dd9
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-02-23 23:20:45 +0000

    Update main-dashboard.js

--- static/scripts/main-dashboard.js
+++ static/scripts/main-dashboard.js
@@ -1,8 +1,18 @@
 window.addEventListener("load", login);
+//window.addEventListener("load", loadNav);
 
 async function login(){
     const url = `/user/session`
     const response = await fetch(url)
     const result = await response.json()
     console.log(result)
+}
+
+async function fetchHtmlAsText(url) {
+    return await (await fetch(url)).text();
+}
+
+
+async function loadNav() {
+    document.getElementById("nav-bar").innerHTML= await fetchHtmlAsText("nav.html");
 }
