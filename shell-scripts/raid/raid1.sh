e98e927dc87ddd834f97813ac4e37c1dacee575d
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-02-26 17:01:05 +0000

    Create raid1.sh

---
+++ shell-scripts/raid/raid1.sh
@@ -0,0 +1,8 @@
+# Key
+# $1 RAID name
+# $2 RAID target
+# $3 RAID dest
+
+sudo mdadm --create --verbose /dev/$1 --level=mirror --raid-devices=2 /dev/$2 /dev/$3
+
+echo UUID=394fd8f2-7b2a-474f-8e58-48b81a6ca8fb /mnt ext4 defaults 0 0 >> /etc/fstab
