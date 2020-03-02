d9b72cb4c603ae8709cd5f2d8f933fb8db76aac8
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-03-02 01:14:47 +0000

    added logout function

--- static/scripts/nav.js
+++ static/scripts/nav.js
@@ -4,6 +4,11 @@ function pageLoad(){
     document.getElementById("logout-button").addEventListener("click", logout)
 }
 
-function logout(){
+async function logout(){
     console.log("logout")
+    const url = `/user/logout`
+    const response = await fetch(url)
+    const result = await response
+    location.replace(result.url)
 }
+
