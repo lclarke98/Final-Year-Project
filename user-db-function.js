64db400a6c75c7d25b2d6e6d3932823249d97af8
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-03-02 01:15:04 +0000

    added verify password

--- user-db-function.js
+++ user-db-function.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2/promise')
 const config = require('./db-config')
+const passwordHash = require('password-hash');
 //const connection = mysql.createConnection(config.mysql)
 const connection = mysql.createConnection(config.mysqlPi)
 //const path = '/Users/leoclarke/Documents/GitHub/Final-Year-Project/api/drive/media/'
@@ -7,7 +8,13 @@ const connection = mysql.createConnection(config.mysqlPi)
 const path = '/media/pi/'
 
 module.exports.loginRequest = async (username, password) => {
+    const empty = []
     let con = await connection
-    let [user] = await con.query("SELECT * FROM user WHERE user_name = ? AND user_password = ?", [username, password])
-    return user
+    let [user] = await con.query("SELECT * FROM user WHERE user_name = ?", [username])
+    console.log(typeof user[0].user_password)
+    if(passwordHash.verify(password.toString(), user[0].user_password) === true){
+        return user
+    }else{
+        return empty
+    }
 };
