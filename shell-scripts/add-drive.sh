3a67a48ab476fcc4f66b4eb6febf3c339725c984
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-03-07 00:23:32 +0000

    fix

--- shell-scripts/add-drive.sh
+++ shell-scripts/add-drive.sh
@@ -1,46 +1,32 @@
-#!/bin/sh
-
+#!/bin/bash
 #KEY
 # $1 drive path
 # $2 drive name
-# $3 permissions
+# $3 read list
+# $4 write list
 
-echo $1
-echo $2
-
-
-newPath="/media/pi/"+$2
-
+#newPath="/media/pi/"+$2
 # Step 0 create mount point
-sudo mkdir /home/pi/nas-mount/$2
-
+#sudo mkdir /home/pi/nas-mount/$2
 # Step get sda and add mount point to fstab
-getPath="$(ls -l /dev/disk/by-label/$2 | tail -c 6)"
-sd="${getPath}" 
-
-sudo mkfs.ext4 /dev/$sd -L $2 -y
-
-sudo echo "/dev$sd /home/pi/nas-mount/$2 ext4 defaults 0 0" >> "/etc/fstab"
-
+#getPath="$(ls -l /dev/disk/by-label/$2 | tail -c 6)"
+#sd="${getPath}" 
+#echo $sd
+#sudo umount /dev/$sd
+#yes| sudo mkfs.ext4 /dev/$sd -L $2
+#sudo echo "/dev$sd /home/pi/nas-mount/$2 ext4 defaults 0 0" >> "/etc/fstab"
 # mount the drive
-sudo mount /home/pi/nas-mount/$2
-
-
+#sudo mount /home/pi/nas-mount/$2
 # Step four change the r+w permissions
-
-sudo chmod a+rwx /home/pi/nas-mount/$2
-
+#sudo chmod a+rwx /home/pi/nas-mount/$2
 # add drive to smb config
-
 smbConf="/etc/samba/smb.conf"
-newPath="/home/pi/nas-mount/$2"
-
-echo "[$2]" >> $smbConf
-echo "path = "$newPath >> $smbConf
-echo "writeable=yes" >> $smbConf
-echo "create mask=0777" >> $smbConf
-echo "directory mask=0777" >> $smbConf
-echo "public=no" >> $smbConf
-
-
-sudo reboot
+newPath="/home/pi/nas-mount/nas"
+sudo echo "[test]" >> "/etc/samba/smb.conf"
+echo "path = /path" >> "/etc/samba/smb.conf"
+echo "writeable=yes" >> "/etc/samba/smb.conf"
+echo "almost there"
+echo "create mask=0777" >> "/etc/samba/smb.conf"
+echo "directory mask=0777" >> "/etc/samba/smb.conf"
+echo "public=no" >> "/etc/samba/smb.conf"
+#sudo reboot
