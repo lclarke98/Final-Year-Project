570085d018bfb37603f3de51206d7bc3c635c040
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-03-07 00:23:40 +0000

    Update shell-script.js

--- api/shell-script.js
+++ api/shell-script.js
@@ -3,27 +3,32 @@ const db = require('../db-function')
 
 function sortReadList(permissions){
     let readList
-    for(let i = 0; i > permissions.length; i++){
-        readlist + " " + permissions[i].read
+    for(let i = 0; i < permissions.length; i++){
+        if(permissions[i].permission_read == 1){
+            readList += " " + permissions[i].user_name
+        }
     }
     return readList
 }
 
 function sortWriteList(permissions){
     let writeList
-    for(let i = 0; i > permissions.length; i++){
-        writelist + " " + permissions[i].write
+    for(let i = 0; i < permissions.length; i++){
+        if(permissions[i].permission_write  == 1){
+            writeList += " " + permissions[i].user_name
+        }
     }
     return writeList
 }
 
-module.exports.addDrive = async (driveName, drivePath, permissions) => {
-    const readList = sortReadList(permissions)
+module.exports.addDrive = async (driveName, drivePath) => {
+    let permissions = await db.getPermissionList(driveName)
+    let readList = sortReadList(permissions)
     console.log(readList)
-    const writeList = sortWriteList(permissions)
+    let writeList = sortWriteList(permissions)
     console.log(writeList)
     console.log("Adding new Drive")
-    shell.exec('sh /home/pi/Final-Year-Project/shell-scripts/add-drive.sh' +" "+ drivePath +" "+ driveName +" "+ readList +" "+ writeList)
+    shell.exec('. /home/pi/Final-Year-Project/shell-scripts/add.sh' +" "+ drivePath +" "+ driveName +" "+ readList +" "+ writeList)
 };
 
 module.exports.addUser = async (userName, password, permissions) => {
