0502fc27a643b57c597a3c859bd4bcb966a6e107
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-03-16 11:59:29 +0000

    Update shell-script.js

--- api/shell-script.js
+++ api/shell-script.js
@@ -1,8 +1,8 @@
 const shell = require('shelljs');
 const db = require('../db-function')
 
-function sortReadList(permissions){
-    let readList = ""
+function sortReadList(driveName, permissions){
+    let readList = driveName + "read"
     for(let i = 0; i < permissions.length; i++){
         if(permissions[i].permission_read == 1){
             console.log(permissions[i].user_name)
@@ -13,8 +13,8 @@ function sortReadList(permissions){
     return readList
 }
 
-function sortWriteList(permissions){
-    let writeList = ""
+function sortWriteList(driveName, permissions){
+    let writeList = driveName + "write"
     for(let i = 0; i < permissions.length; i++){
         if(permissions[i].permission_write  == 1){
             writeList += " " + permissions[i].user_name
@@ -25,11 +25,10 @@ function sortWriteList(permissions){
 
 module.exports.addDrive = async (driveName, drivePath) => {
     let permissions = await db.getPermissionList(driveName)
-    let readList = "'" + sortReadList(permissions) + "'" 
+    let readList = "'" + sortReadList(driveName, permissions) + "'" 
     //console.log(readList)
-    let writeList = "'" + sortWriteList(permissions) + "'"
-    //console.log(writeList)
-    //console.log("Adding new Drive")
+    let writeList = "'" + sortWriteList(driveName, permissions) + "'"
+
     shell.exec('sh /home/pi/shell/add-drive.sh' +" "+ drivePath +" "+ driveName +" "+ readList +" "+ writeList)
     //shell.exec('sh /home/pi/Final-Year-Project/shell-scripts/add.sh' +" "+ "helloWorld")
 };
@@ -38,10 +37,17 @@ module.exports.addNewFolder = async (folderName) => {
     shell.exec('sh /home/pi/shell/new-folder.sh' +" "+ folderName)
 };
 
+
+
 module.exports.addUser = async (userName, password, permissions) => {
     shell.exec('sh ../shell-scripts/add-user.sh' + userName, password)
+    updatePermissions(userName)
 };
 
 module.exports.deleteUser = async (userName) => {
     shell.exec('sh ../shell-scripts/delete-user.sh' + userName)
 };
+
+async function updatePermissions(userName){
+    
+}
