81c3f80b18749a7f930806125d195272d4919885
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-03-17 20:08:16 +0000

    Update update-permissions.sh

--- shell-scripts/update-permissions.sh
+++ shell-scripts/update-permissions.sh
@@ -4,6 +4,6 @@
 # $1 comment line
 # $2 new permissions line
 
-sed -i '/'$1'/c\'$2'' /etc/samab/smb.conf
+sed -i "/$1/c\ read list=  $2" /etc/samba/smb.conf
 
 path = /home/pi/nas-mount/NAS1         #nas1-path
