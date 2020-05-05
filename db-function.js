7fedc6b9de6b8daa22397eb74259709a39932c27
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-05-05 22:21:23 +0100

    Update db-function.js

--- db-function.js
+++ db-function.js
@@ -48,7 +48,7 @@ module.exports.addNewDrive = async (name, path) => {
         let con = await connection
         let newPath = '/home/pi/Final-Year-Project/static/nas-mount/' + name
         console.log(newPath)
-        await con.query("INSERT INTO addedDrive(addedDrive_name) VALUES (?,?)", [name, newPath]);
+        await con.query("INSERT INTO addedDrive(addedDrive_name, addedDrive_path) VALUES (?,?)", [name, newPath]);
         await con.query("UPDATE unaddedDrive SET unaddedDrive_added = ? WHERE unaddedDrive_path = ? ",[true, path])
         let [id] = await con.query("SELECT addedDrive_name FROM addedDrive WHERE addedDrive_name = ? ",[name])
         return id
